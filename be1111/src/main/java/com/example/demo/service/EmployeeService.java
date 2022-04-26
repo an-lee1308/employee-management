ca2c672ac3e2eec19845284dc0d2c9b9f9ff11bf
AnLe <nhoxbuonlv3@gmail.com>
2022-04-26 17:39:02 +0700

    test custom findByName in repository

--- be1111/src/main/java/com/example/demo/service/EmployeeService.java
+++ be1111/src/main/java/com/example/demo/service/EmployeeService.java
@@ -26,9 +26,31 @@ public class EmployeeService implements INewService {
 
         // we need to check whether employee with given id is exist in DB or not
 
+//        EmployeeModel employeeInDb=employeeRepository.getById(id);
+//        if(employeeInDb == null) {
+//            return ResponseEntity.notFound().build();
+//        }
+//        employeeInDb.setFullName(EmployeeModel.getFullName());
+//        employeeInDb.setAge(EmployeeModel.getAge());
+
+        EmployeeModel existingEmployee = employeeRepository.getById(id);
+        existingEmployee.setFullName(employee.getFullName());
+        existingEmployee.setAge(employee.getAge());
+        existingEmployee.setGender(employee.getGender());
+        existingEmployee.setAddress(employee.getAddress());
+
+        existingEmployee.setPhoneNumber(employee.getPhoneNumber());
+        existingEmployee.setStartDay(employee.getStartDay());
+        existingEmployee.setMoneyPerHour(employee.getMoneyPerHour());
+        existingEmployee.setTotalHours(employee.getTotalHours());
+        existingEmployee.setImageURL(employee.getImageURL());
+        existingEmployee.setTeamID(employee.getTeamID());
+
+
+
         // save existing employee to DB
-        employeeRepository.save(employee);
-        return employee;
+        employeeRepository.save(existingEmployee);
+        return existingEmployee;
     }
     @Override
     public void deleteEmployee(long id) {
@@ -36,9 +58,15 @@ public class EmployeeService implements INewService {
         // check whether a employee exist in a DB or not
         employeeRepository.deleteById(id);
     }
-//    @Override
-//    public List<EmployeeModel> findEmployeeByName(String name)  {
-//        return employeeRepository.findAll(name);
-//    }
 
+    @Override
+    public List<EmployeeModel> findByName(String name) {
+        return  employeeRepository.findByFullName(name);
+    }
+
+
+@Override
+public EmployeeModel getEmployeeById(long id)       {
+    return employeeRepository.findById(id).get(); //No serializer found for class org.hibernate.proxy.pojo.bytebuddy.ByteBuddyInterceptor
+}
 }
