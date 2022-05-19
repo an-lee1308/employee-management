7afa2b0e0f4884efe7047d83591d9c6f2d0d9839
anlee1308 <nhoxbuonlv3@gmail.com>
2022-05-20 00:01:21 +0700

    display FORMDATA

--- fe-typescript/src/components/HeadContainer/HeadContainer.jsx
+++ fe-typescript/src/components/HeadContainer/HeadContainer.jsx
@@ -7,12 +7,26 @@ import './HeadContainer.scss';
 import axios from 'axios';
 import { useEffect } from 'react';
 
+// const formData = new formData();
+
 function HeadContainer() {
 	// type Props = {
 	//     some?: any,
 	//     style?: string,
 	//     Modal?: any
 	// };
+
+	const [valueForm, setvalueForm] = useState({
+		fullname: '',
+		age: '',
+		address: '',
+		gender: '',
+		phonenumber: '',
+		startday: '',
+		totalhours: '',
+		moneyperhour: '',
+		employeeteam: '',
+	});
 	const [teamList, setTeamList] = useState([]);
 	useEffect(() => {
 		async function getTeamList() {
@@ -24,10 +38,74 @@ function HeadContainer() {
 		}
 		getTeamList();
 	}, []);
+	console.log(valueForm);
+	function onChangeFormSelect(e) {
+		console.log(e);
+		setvalueForm({
+			...valueForm,
+			gender: e,
+		});
+	}
+
+	function onChangeFormUpload(e) {
+		console.log(e.file.originFileObj);
+		if (e.fileList.length > 0) {
+			var src = URL.createObjectURL(e.file.originFileObj);
+			console.log('Lofg src', src);
+		}
+		setvalueForm({
+			...valueForm,
+			image: e.file.originFileObj,
+		});
+	}
+
+	const dummyRequest = ({ file, onSuccess }) => {
+		setTimeout(() => {
+			onSuccess('ok');
+		}, 0);
+	};
+
+	function onChangeFormSelectTeam(e) {
+		console.log(e);
+		setvalueForm({
+			...valueForm,
+			employeeteam: e,
+		});
+	}
+
+	function onChangeDatePicker(e) {
+		console.log(e._d);
+		setvalueForm({
+			...valueForm,
+			startday: e._d,
+		});
+	}
+
+	function onChangeForm(e) {
+		setvalueForm({
+			...valueForm,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	function resetForm() {
+		setvalueForm({
+			fullname: '',
+			age: '',
+			address: '',
+			gender: '',
+			phonenumber: '',
+			startday: '',
+			totalhours: '',
+			moneyperhour: '',
+			employeeteam: '',
+		});
+	}
+
 	const handleDelete = () => {
 		console.log('hihihihi');
 	};
-	const { Option } = Select;
+	// const { Option } = Select;
 	const formItemLayout = {
 		labelCol: {
 			span: 6,
@@ -39,12 +117,13 @@ function HeadContainer() {
 
 	const normFile = (e) => {
 		console.log('Upload event:', e);
+		console.log('Test', e.file);
 
-		if (Array.isArray(e)) {
-			return e;
-		}
+		// if (Array.isArray(e)) {
+		// 	return e;
+		// }
 
-		return e && e.fileList;
+		// return e && e.fileList;
 	};
 
 	const onFinish = (values) => {
@@ -58,7 +137,35 @@ function HeadContainer() {
 	};
 
 	const handleOk = () => {
-		setIsModalVisible(false);
+		console.log(valueForm);
+		// const isEmpty = Object.values(valueForm).some((x) => x === '');
+		const isEmpty = Object.values(valueForm).every((x) => x !== '');
+		console.log(isEmpty);
+		const form = new FormData();
+		if (isEmpty) {
+			console.log('value fornm', valueForm);
+			console.log('đủ trường thì nhảy vào đây');
+			form.append('fullName', valueForm.fullname);
+			form.append('age', valueForm.age);
+			form.append('address', valueForm.address);
+			form.append('gender', valueForm.gender);
+			form.append('phoneNumber', valueForm.phonenumber);
+			form.append('moneyPerhour', valueForm.moneyperhour);
+			form.append('startDay', valueForm.startday);
+			form.append('totalHours', valueForm.totalhours);
+			form.append('file', valueForm.image);
+			form.append('employeeTeam', valueForm.employeeteam);
+			for (var pair of form.entries()) {
+				console.log(pair[0] + ', ' + pair[1]);
+			}
+		} else {
+			// setIsModalVisible(false);
+			// resetForm();
+
+			// var form = new FormData();
+
+			console.log('văng');
+		}
 	};
 
 	const handleCancel = () => {
@@ -88,6 +195,7 @@ function HeadContainer() {
 				visible={isModalVisible}
 				onOk={handleOk}
 				onCancel={handleCancel}
+				destroyOnClose={true}
 			>
 				<Form
 					name='validate_other'
@@ -99,51 +207,110 @@ function HeadContainer() {
 						rate: 3.5,
 					}}
 				>
-					<Form.Item label='Fullname employee'>
-						<Input onChange={(e) => console.log(e.target.value)} required />
+					<Form.Item
+						label='Fullname employee'
+						onBlur={onChangeForm}
+						validateStatus={valueForm.fullname === '' ? 'error' : 'validating'}
+						help={
+							valueForm.fullname === '' &&
+							'Should be combination of numbers & alphabets'
+						}
+					>
+						<Input
+							onChange={onChangeForm}
+							name='fullname'
+							value={valueForm.fullname}
+						/>
 					</Form.Item>
-
+					{/* <Form.Item label='upload file'>
+						<Input
+							type='file'
+							onChange={(e) => console.log(e.target.files[0])}
+						/>
+					</Form.Item> */}
 					<Form.Item label='Address'>
-						<Input onChange={(e) => console.log(e.target.value)} />
+						<Input
+							onChange={onChangeForm}
+							name='address'
+							value={valueForm.address}
+						/>
 					</Form.Item>
 					<Form.Item label='Sex employee'>
-						<Select onChange={(e) => console.log(e)}>
+						<Select
+							onChange={onChangeFormSelect}
+							// name='gender'
+							value={valueForm.gender}
+						>
 							<Select.Option value='Male'>Male</Select.Option>
 							<Select.Option value='Female'>Female</Select.Option>
 						</Select>
 					</Form.Item>
 
 					<Form.Item label='Age'>
-						<Input onChange={(e) => console.log(e.target.value)} />
+						<Input
+							onChange={onChangeForm}
+							name='age'
+							// type={'number'}
+							value={valueForm.age}
+						/>
 					</Form.Item>
 					<Form.Item label='Start day'>
-						<DatePicker onChange={(e) => console.log(e._d)} />
+						<DatePicker
+							onChange={onChangeDatePicker}
+							name='startday'
+							// type={'number'}
+						/>
 					</Form.Item>
 
 					<Form.Item label='Money/hour'>
-						<Input onChange={(e) => console.log(e.target.value)} />
+						<Input
+							onChange={onChangeForm}
+							name='moneyperhour'
+							// type={'number'}
+							value={valueForm.moneyperhour}
+						/>
 					</Form.Item>
 
 					<Form.Item
-						name='upload'
+						// name='upload'
 						label='Upload image'
-						valuePropName='fileList'
-						getValueFromEvent={normFile}
+						// valuePropName='fileList'
+						// getValueFromEvent={normFile}
+						// onChange={(e) => console.log(e)}
 					>
-						{/* <Upload name='logo' action='/upload.do' listType='picture'> */}
-						<Upload>
+						{/* <Upload name='logo' action='/upload.do' listType='picture'></Upload> */}
+						<Upload
+							listType='picture'
+							action={''}
+							// accept='.png,.jpeg'
+							onChange={onChangeFormUpload}
+							customRequest={dummyRequest}
+						>
 							<Button icon={<UploadOutlined />}>Click to upload</Button>
 						</Upload>
 					</Form.Item>
 					<Form.Item label='Phone number'>
-						<Input onChange={(e) => console.log(e.target.value)} />
+						<Input
+							onChange={onChangeForm}
+							name='phonenumber'
+							// type={'number'}
+							value={valueForm.phonenumber}
+						/>
 					</Form.Item>
 
 					<Form.Item label='Total hours'>
-						<Input onChange={(e) => console.log(e.target.value)} />
+						<Input
+							onChange={onChangeForm}
+							name='totalhours'
+							// type={'number'}
+							value={valueForm.totalhours}
+						/>
 					</Form.Item>
 					<Form.Item label='Team'>
-						<Select onChange={(e) => console.log(e)}>
+						<Select
+							onChange={onChangeFormSelectTeam}
+							value={valueForm.employeeteam}
+						>
 							{teamList &&
 								teamList.length > 0 &&
 								teamList.map((team, index) => {
